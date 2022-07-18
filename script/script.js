5e0d18f53946d2201861d317cf8e2fd3b93e2105
Vyacheslav2123 <72310551+Vyacheslav2123@users.noreply.github.com>
2022-07-19 00:00:50 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const check = document.querySelector('.main');
+const menu__box = document.querySelector('.menu__box');
+
+check.addEventListener('click', event => {
+    menu__toggle.checked = false;
+});
+menu__box.addEventListener('click', event => {
+    menu__toggle.checked = false;
+});
