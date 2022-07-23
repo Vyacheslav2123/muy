8576b57f1e6bdf6007c9ea5c01f10ee930c12a3c
Vyacheslav2123 <72310551+Vyacheslav2123@users.noreply.github.com>
2022-07-24 00:43:34 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -8,6 +8,17 @@ menu__box.addEventListener('click', event => {
     menu__toggle.checked = false;
 });
 
+function reg() {
+    document.getElementById("reg").classList.add('reg');
+    document.getElementById("entry").classList.add('delete');
+}
+function entry () {
+    document.getElementById("reg").classList.add('delete');
+    document.getElementById("entry").classList.remove('delete');
+}
+
+
+
 AOS.init({
     disable: window.innerWidth < 1024
 });
